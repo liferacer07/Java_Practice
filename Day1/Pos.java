2273b1473a873f9427c949dff37b75c4fa384c35
liferacer07 <tejashshende7@gmail.com>
2024-07-18 22:18:07 +0530

    Add files via upload

--- Day1/Pos.java
+++ Day1/Pos.java
@@ -1,1 +1,25 @@
-hii
+package day_1;
+
+/*Write a java program to check wheather a number is positive negative or zero..
+*/
+
+import java.util.*;
+class Pos{
+public static void main(String[]  args){
+
+
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter number:");
+int num=sc.nextInt();
+
+if(num>0){
+System.out.println("Positive number="+num);
+}
+else if(num<0){
+System.out.println("Negative number="+num);
+}
+else{
+System.out.println("Zero");
+}
+
+}}
---
+++ Day1/Sample.java
@@ -0,0 +1,10 @@
+package day_1;
+
+public class Sample{
+public static void main(String [] teju){
+int a=10,b=20,sum;
+sum=a+b;
+
+System.out.println("Addition="+(a+b));
+
+}}
